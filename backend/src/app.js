3fed7d8dd7e46ddfed366ce6942d827cda0ad262
Lahmar22 <lahmarzakariae22@gmail.com>
2026-06-02 17:07:33 +0100

    feat : updated the app and server for add routes and db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
 
 
 const app = express();
@@ -7,6 +8,6 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,28 @@
 require("dotenv").config();
 
 const app = require("./app");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger/swaggerConfig");
 
-
-const connectDB = require("./config/db");
+const {connectDB} = require("./config/db");
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, {
+        customSiteTitle: "API Documentation",
+        customCss: `
+        .topbar-wrapper { display: none; }
+        .swagger-ui .topbar { display: none; }
+        .swagger-ui .info { margin: 20px 0; }
+        .swagger-ui .info .title { color: #3b4151; }
+    `,
+    })
+);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

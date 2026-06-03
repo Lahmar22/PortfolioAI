711cc5423f29ac390f94eb6e1f33eba0d6efcf87
Lahmar22 <lahmarzakariae22@gmail.com>
2026-06-03 12:31:27 +0100

    feat : updated the app for route the profile

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
 
 const app = express();
@@ -9,5 +10,5 @@ app.use(cors());
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-
+app.use("/api/profile", profileRoutes);
 module.exports = app;

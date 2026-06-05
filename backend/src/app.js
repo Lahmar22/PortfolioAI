37709fee8dd081508cf756a5d1696576460118c1
Lahmar22 <lahmarzakariae22@gmail.com>
2026-06-05 12:42:26 +0100

    feat : updated the app for added the routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
-
+const templateRoutes = require("./routes/templatRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const skillsRoutes = require("./routes/skillsRoutes");
+const educationRoutes = require("./routes/educationRoutes");
+const certificateRoutes = require("./routes/certificateRoutes");
+const experienceRoutes = require("./routes/experienceRoutes");
+const socialRoutes = require("./routes/socialRoutes");
 
 const app = express();
 
@@ -11,4 +17,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/profile", profileRoutes);
+app.use("/api/templates", templateRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/skills", skillsRoutes);
+app.use("/api/education", educationRoutes);
+app.use("/api/certificates", certificateRoutes);
+app.use("/api/experiences", experienceRoutes);
+app.use("/api/social-links", socialRoutes);
 module.exports = app;

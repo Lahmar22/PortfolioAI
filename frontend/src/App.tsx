7aa54022988c9631591ee4b080a6f5725cc4b3e1
Lahmar22 <lahmarzakariae22@gmail.com>
2026-06-05 16:20:56 +0100

    faet : update the app and index css for tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { AuthProvider } from "./context/AuthContext";
 import Router from "./routes";
 
 function App() {
-  return <Router />;
+  return (
+    <AuthProvider>
+      <Router />
+    </AuthProvider>
+  );
 }
 
 export default App;
